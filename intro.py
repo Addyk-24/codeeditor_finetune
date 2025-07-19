5567e11abf9b38ae037bdbb7961f00112f1f6481
Addyk-24 <addykatkar24@gmail.com>
2025-07-19 15:29:34 +0530

    added

--- intro.py
+++ intro.py
@@ -2,13 +2,20 @@ from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
 
-def tokenize_text(text):
-    tokenized_text = tokenizer(text)["input_ids"]
-    return tokenized_text
+# WITHOUT PADDING AND EOS
+# def tokenize_text(text):
+#     tokenized_text = tokenizer(text)["input_ids"]
+#     return tokenized_text
 
+def tokenize_text_with_padding(text):
+    # tokenizer.pad_token = tokenizer.eos_token since bert model deos not have eos token so this line does not affect
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    tokenized_text = tokenizer(text,padding=True)["input_ids"]
+    return tokenized_text
 def main():
     sample_text = "Hi, This is Aditya and I like to explore new technologies."
-    tokenized_output = tokenize_text(sample_text)
+    # tokenized_output = tokenize_text(sample_text)
+    tokenized_output = tokenize_text_with_padding(sample_text)
     print("Tokenized Output:", tokenized_output)
 
 if __name__ == "__main__":
