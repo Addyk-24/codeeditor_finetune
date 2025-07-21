307413b9aefb4f5eac6ad0c4aa7a373058b07bc3
Addyk-24 <addykatkar24@gmail.com>
2025-07-21 22:49:05 +0530

    Added New

--- train_process.-02.py
+++ train_process.-02.py
@@ -1,9 +1,9 @@
-from utilities import *
+# from utilities import *
 from transformers import AutoTokenizer
 from transformers import AutoModelForCausalLM
 from transformers import TrainingArguments
 from transformers import AutoModelForCausalLM
-from llama import BasicModelRunner
+# from llama import BasicModelRunner
 
 
 import time
@@ -71,3 +71,46 @@ else:
 #   generated_text_answer = generated_text_with_prompt[0][len(text):]
 
 #   return generated_text_answer
+
+
+# TRAINING ARGUMENTS
+# max_steps = 3
+# training_args = TrainingArguments(
+
+# # Learning rate
+# learning_rate=1.0e-5,
+
+# # Number of training epochs
+# num_train_epochs=1,
+
+# # Max steps to train for (each step is a batch of data)
+# # Overrides num_train_epochs, if not -1
+# max_steps=max_steps,
+
+# # Batch size for training
+# per_device_train_batch_size=1,
+
+# # Directory to save model checkpoints
+# output_dir=output_dir,
+
+# # Other arguments
+# overwrite_output_dir=False, # Overwrite the content of the output directory
+# disable_tqdm=False, # Disable progress bars
+# eval_steps=120, # Number of update steps between two evaluations
+# save_steps=120, # After # steps model is saved
+# warmup_steps=1, # Number of warmup steps for learning rate scheduler
+# per_device_eval_batch_size=1, # Batch size for evaluation
+# evaluation_strategy="steps",
+# logging_strategy="steps",
+# logging_steps=1,
+# optim="adafactor",
+# gradient_accumulation_steps = 4,
+# gradient_checkpointing=False,
+
+# # Parameters for early stopping
+
+# load_best_model_at_end=True,
+# save_total_limit=1,
+# metric_for_best_model="eval_loss",
+# greater_is_better=False
+# )
