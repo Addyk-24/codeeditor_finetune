3b3d0064665130d2e6ad7ada79c771aa48a20369
Addyk-24 <addykatkar24@gmail.com>
2025-07-22 22:27:21 +0530

    AddedNeww

--- main-finetune.py
+++ main-finetune.py
@@ -8,9 +8,14 @@ tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
 
 dataset = load_dataset("mteb/tweet_sentiment_extraction")
 
-for i in range(len(dataset["test"])):
-    print("Dataset:",dataset["test"]["text"][i])
+# for i in range(len(dataset["test"])):
+#     print("Dataset:",dataset["test"]["text"][i])
 
 def tokenize_func(text):
-    
+    return tokenizer(text["test"],truncation=True)["input_ids"]
+
+tokenized_dataset = dataset.map(tokenize_func,batched=True)
+
+print("Tokenized Dataset: \n", tokenized_dataset["test"]["input_ids"][0])
+
 
