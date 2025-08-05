a8ea9445d08180690c8e833c5eb748c5202f85bf
Addyk-24 <addykatkar24@gmail.com>
2025-08-05 11:12:23 +0530

    Model evaluation and iteration

---
+++ evalanditer.py
@@ -0,0 +1,33 @@
+# There is few steps to run the evaluation in GPUs using Lamini
+
+# finetuned_model = BasicModelRunner(
+#     "lamini/lamini_docs_finetuned"
+# )
+# finetuned_output = finetuned_model(
+#     test_dataset_list # batched!
+# ) 
+
+# LETS THIS UNDER THE HOOD
+
+import datasets
+import tempfile
+import logging
+import random
+import config
+import os
+import yaml
+import logging
+import difflib
+import pandas as pd
+
+import transformers
+import datasets
+import torch
+
+from tqdm import tqdm
+from utilities import *
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+logger = logging.getLogger(__name__)
+global_config = None
+
